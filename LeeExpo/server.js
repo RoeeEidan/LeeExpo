2b9ff900013abc3fdc2cc98297fc80b6e765ba3f
Roee Eidan <Roeeeidan@gmail.com>
2017-03-11 13:26:02 -0500

    adding port

--- LeeExpo/server.js
+++ LeeExpo/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendfile('./public/index.html')
 })
 
-app.listen(8080 , function () {
-  console.log('Example app listening on port 8080!')
+app.listen(PORT , function () {
+  console.log('Example app listening on port '+PORT)
 })
