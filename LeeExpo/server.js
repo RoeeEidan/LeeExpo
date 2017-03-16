aa46b378cffc357af398015552370a7b599c3af5
Roee Eidan <Roeeeidan@gmail.com>
2017-03-16 12:10:02 -0400

    URLSS

--- LeeExpo/server.js
+++ LeeExpo/server.js
@@ -6,7 +6,22 @@ const PORT = process.env.PORT || 80;
 app.use(express.static(__dirname + '/build'));
 
 app.get('*', function (req, res) {
-  console.log('get endpoint')
+  console.log('get endpoint *')
+  res.sendfile('./build/index.html', {root: __dirname + '/build'})
+})
+
+app.get('/', function (req, res) {
+  console.log('get endpoint /')
+  res.sendfile('./build/index.html', {root: __dirname + '/build'})
+})
+
+app.get('ec2-52-37-31-115.us-west-2.compute.amazonaws.com', function (req, res) {
+  console.log('get endpoint ec2-52-37-31-115.us-west-2.compute.amazonaws.com')
+  res.sendfile('./build/index.html', {root: __dirname + '/build'})
+})
+
+app.get('http://52.37.31.115/', function (req, res) {
+  console.log('get endpoint http://52.37.31.115/')
   res.sendfile('./build/index.html', {root: __dirname + '/build'})
 })
 
