12c0edd0bba1c3b2c2aa309bebd78b101ee830f2
Roee Eidan <Roeeeidan@gmail.com>
2017-03-16 11:57:55 -0400

    Trying

--- LeeExpo/server.js
+++ LeeExpo/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/build'));
 
-app.get('http://52.37.31.115/', function (req, res) {
+app.get('/', function (req, res) {
   res.sendfile('./build/index.html', {root: __dirname + '/build'})
 })
 
